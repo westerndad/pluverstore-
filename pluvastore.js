998dcae40c71187807babd4666afeb68441a2a59
westerndad <69368997+westerndad@users.noreply.github.com>
2020-08-08 10:25:36 +0100

    Add  initial pluverstore website files

---
+++ pluvastore.js
@@ -0,0 +1,13 @@
+// const item = document.getElementsByClassName('collection-item')
+// console.log(item[3])
+// item[3].textContent = "James Jones"
+// item[2].style.background= 'red';
+
+
+// let items = document.querySelector('ul').getElementsByClassName('li');
+// console.log(items)
+// items[2].style.color =  'orange'; 
+let item = document.querySelector('him').getElementsByClaName('li')
+ item = Array.from(item)
+item.reverse();
+console.log(item)
